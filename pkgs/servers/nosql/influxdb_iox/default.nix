717620c7c99abcad4b53e0fe050f8986cbde79d8
nviets <nathan.viets@gmail.com>
2023-01-22 13:30:51 -0600

    fails on GIT_HASH error. added link to possible fix

--- pkgs/servers/nosql/influxdb_iox/default.nix
+++ pkgs/servers/nosql/influxdb_iox/default.nix
@@ -22,25 +22,33 @@ rustPlatform.buildRustPackage {
       rev = "840923abab56f5f5f7fedf2a622e5f976a3d9eac";
       sha256 = "sha256-ZBxs0CuyoAQP5+yEjlLPziIc+u83i64Q/+RgSiU8eWc=";
     };
-    #sourceRoot = "source/libflux";
+    #sourceRoot = "source/influxdb_iox";
     cargoSha256 = "sha256-RXAdTBtD0b/iEToJuefQFJSZAsTQ+TqODVFlA6VvuNk=";
     nativeBuildInputs = [ llvmPackages.libclang protobuf ];
-    buildInputs = lib.optional stdenv.isDarwin libiconv;
-    LIBCLANG_PATH = "${llvmPackages.libclang.lib}/lib";
-    pkgcfg = ''
-      Name: influxdb_iox
-      Version: "dev"
-      Description: Influx IOX
+    buildInputs = [ llvmPackages.libclang protobuf ] ++ lib.optional stdenv.isDarwin libiconv;
+    #LIBCLANG_PATH = "${llvmPackages.libclang.lib}/lib";
+#    pkgcfg = ''
+#      Name: influxdb_iox
+#      Version: "dev"
+#      Description: Influx IOX
+#      Cflags: -I/out/include
+#      Libs: -L/out/lib -ldl -lpthread
+#    '';
+    configurePhase = ''
+      # https://github.com/NixOS/nixpkgs/blob/c3cc2bded1d080e20fa2b2b546bf04f19eef5cd9/pkgs/servers/mail/vsmtp/default.nix
+      # see this page for an example of GIT_HASH patch
+      cat influxdb_iox/build.rs
+      set RUST_BACKTRACE=1
     '';
-    passAsFile = [ "pkgcfg" ];
-    postInstall = ''
-      mkdir -p $out/include $out/pkgconfig
-      cp -r $NIX_BUILD_TOP/source/libflux/include/influxdata $out/include
-      substitute $pkgcfgPath $out/pkgconfig/flux.pc \
-        --replace /out $out
-    '' + lib.optionalString stdenv.isDarwin ''
-      install_name_tool -id $out/lib/libflux.dylib $out/lib/libflux.dylib
-    '';
+#    passAsFile = [ "pkgcfg" ];
+#    postInstall = ''
+#      mkdir -p $out/include $out/pkgconfig
+#      cp -r $NIX_BUILD_TOP/source/libflux/include/influxdata $out/include
+#      substitute $pkgcfgPath $out/pkgconfig/flux.pc \
+#        --replace /out $out
+#    '' + lib.optionalString stdenv.isDarwin ''
+#      install_name_tool -id $out/lib/libflux.dylib $out/lib/libflux.dylib
+#    '';
 
   meta = with lib; {
     description = "An open-source distributed time series database";
