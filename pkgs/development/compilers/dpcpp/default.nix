cf882e07a7e32a814fe29524477ccc907c8216df
nviets <nathan.viets@gmail.com>
2023-04-07 17:45:44 -0500

    refortmatted a bit. fails for opteron machine. wip.

--- pkgs/development/compilers/dpcpp/default.nix
+++ pkgs/development/compilers/dpcpp/default.nix
@@ -5,6 +5,8 @@
 , cmake
 , ninja
 , python3
+, spirv-headers
+#, level-zero
 }:
 let
   vc-intrinsics = fetchFromGitHub {
@@ -27,17 +29,25 @@ let
     rev = "v2023.02.06";
     hash = "sha256-uWPwGznfqH0xvrpnVNdDn3H3tnAfJt9A3m6av0xlq7I=";
   };
-  spirv = fetchFromGitHub {
-    owner = "KhronosGroup";
-    repo = "SPIRV-Headers";
-    rev = "sdk-1.3.243.0";
-    hash = "sha256-VOq3r6ZcbDGGxjqC4IoPMGC5n1APUPUAs9xcRzxdyfk=";
-  };
   mp11 = fetchFromGitHub {
     owner = "boostorg";
     repo = "mp11";
     rev = "boost-1.81.0";
     hash = "sha256-rPr7uVGAc4x8vwbx/LhhWH6pcH2t9mS2q9WVL8vpTwQ=";
+  };
+  level-zero = fetchFromGitHub {
+    owner = "oneapi-src";
+    repo = "level-zero";
+    rev = "v1.8.8";
+    hash = "sha256-hfbTgEbvrhWkZEi8Km7KaxJBAc9X1kA/T2DLooKa7KQ=";
+  };
+  unified-runtime = fetchFromGitHub {
+    owner = "oneapi-src";
+    repo = "unified-runtime";
+    #rev = "v0.6";
+    rev = "d6af758779db6eebdc419fd5e249302f566eb5de";
+    #hash = "sha256-uu1Dw1y4C7tIuUph/uNwp+DwwubDgTEkFD1g32Cm/2I=";
+    hash = "sha256-9ZIUZ2ZqEv9bcJKlItfXlfMdD968UCGIDLUTL3PmU3o=";
   };
 in
 stdenv.mkDerivation rec {
@@ -50,7 +60,7 @@ stdenv.mkDerivation rec {
     owner = "intel";
     repo = "llvm";
     rev = "cb91c232c661829b327c7e6e8232eb1d79100a98";
-    hash = "sha256-uchUba3llgT+zJTNb2F/tBMuJJnJw+L/00vhfTwMBTQ=";
+    hash = "sha256-gilApWrqIqKkqLDCpS7FHdWkT0j8LjCZpiQmqQjuos0=";
     deepClone = true;
   };
 
@@ -58,13 +68,27 @@ stdenv.mkDerivation rec {
     cmake
     ninja
     python3
+    spirv-headers
+    #level-zero
   ];
 
   configurePhase = ''
+    export LEVEL_ZERO_LIBRARY=${level-zero}
     substituteInPlace buildbot/configure.py \
-      --replace '"cmake",' '"cmake", "-DLLVMGenXIntrinsics_SOURCE_DIR=${vc-intrinsics}", "-DOpenCL_HEADERS=${ocl-headers}", "-DOpenCL_LIBRARY_SRC=${ocl-loader}", "-DBOOST_MP11_SOURCE_DIR=${mp11}", "-DLLVM_EXTERNAL_SPIRV_HEADERS_SOURCE_DIR=${spirv}"'
+      --replace '"cmake",' \
+      '"cmake",
+      "-DLLVMGenXIntrinsics_SOURCE_DIR=${vc-intrinsics}",
+      "-DOpenCL_HEADERS=${ocl-headers}",
+      "-DOpenCL_LIBRARY_SRC=${ocl-loader}",
+      "-DBOOST_MP11_SOURCE_DIR=${mp11}",
+      "-DLLVM_EXTERNAL_SPIRV_HEADERS_SOURCE_DIR=${spirv-headers}",
+      "-DUNIFIED_RUNTIME_LIBRARY=${unified-runtime}",
+      "-DUNIFIED_RUNTIME_INCLUDE_DIR=${unified-runtime}/include",'
+    cat buildbot/configure.py
     echo START
-    python buildbot/configure.py
+    python buildbot/configure.py \
+      --l0-headers ${level-zero}/include \
+      --l0-loader ${level-zero}
     echo FINISH
   '';
 
