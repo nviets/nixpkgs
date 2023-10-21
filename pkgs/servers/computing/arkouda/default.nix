cfd7e5243ce0d270f9e86740d3ecd16e79788e4a
nviets <nathan.viets@gmail.com>
2023-10-21 18:53:21 -0500

    builds but install phase needs work

--- pkgs/servers/computing/arkouda/default.nix
+++ pkgs/servers/computing/arkouda/default.nix
@@ -1,5 +1,6 @@
 { lib, stdenv, fetchFromGitHub, pkg-config
-, chapel, zeromq, hdf5, arrow-cpp, iconv, libiconv, glibc, libidn2, gmp}:
+, chapel, zeromq, hdf5, arrow-cpp, iconv, libiconv, glibc, libidn2, gmp
+, curl, libiconvReal}:
 
 stdenv.mkDerivation rec {
   pname = "arkouda";
@@ -15,26 +16,43 @@ stdenv.mkDerivation rec {
 
   #outputs = [ "out" ];
 
-  nativeBuildInputs = [ pkg-config ];
-
   preConfigure = ''
     export ARKOUDA_ZMQ_PATH=${zeromq}
     export ARKOUDA_HDF5_PATH=${hdf5.dev}
     export ARKOUDA_ARROW_PATH=${arrow-cpp}
-    export ARKOUDA_ICONV_PATH=${libiconv}
+    #export ARKOUDA_ICONV_PATH=${iconv}
     export ARKOUDA_IDN2_PATH=${libidn2}
     export ARKOUDA_SKIP_CHECK_DEPS=yes
+    export LD_LIBRARY_PATH=${lib.makeLibraryPath [ glibc libiconv ]}
+
+    echo "ZMQ_PATH=${zeromq}" >> Makefile.paths
+    echo "HDF5_PATH=${hdf5.dev}" >> Makefile.paths
+    echo "ARROW_PATH=${arrow-cpp}" >> Makefile.paths
+    #echo "ICONV_PATH=${libiconv}" >> Makefile.paths
+    echo "IDN2_PATH=${libidn2}" >> Makefile.paths
+    cat Makefile.paths
+
+    echo CHECKWHERESTUFFIS
+    ls
+    #export NIX_LDFLAGS+=" -L${iconv}"
+    echo $NIX_LDFLAGS
+    #export CHPL_FLAGS="-I${libiconv}/include"
+
+    substituteInPlace Makefile \
+      --replace "Checking for ZMQ" "Checking for ZMQ: \$(CHPL_FLAGS)"
   '';
 
-  cmakeFlags = [
-    "-DARKOUDA_ZMQ_PATH=${zeromq}"
-    "-DARKOUDA_HDF5_PATH=${hdf5.dev}"
-    "-DARKOUDA_ARROW_PATH=${arrow-cpp}"
-    "-DARKOUDA_ICONV_PATH=${libiconv}"
-    "-DARKOUDA_IDN2_PATH=${libidn2}"
-  ];
+#  cmakeFlags = [
+#    "-DARKOUDA_ZMQ_PATH=${zeromq}"
+#    "-DARKOUDA_HDF5_PATH=${hdf5.dev}"
+#    "-DARKOUDA_ARROW_PATH=${arrow-cpp}"
+#    "-DARKOUDA_ICONV_PATH=${libiconv}"
+#    "-DARKOUDA_IDN2_PATH=${libidn2}"
+#  ];
 
-  buildInputs = [ chapel zeromq hdf5.dev arrow-cpp iconv libiconv glibc libidn2 ];
+  nativeBuildInputs = [ pkg-config ];
+
+  buildInputs = [ chapel zeromq hdf5.dev arrow-cpp libiconv libidn2 curl libiconvReal ];
 
   propagatedBuildInputs = [ gmp ];
 
